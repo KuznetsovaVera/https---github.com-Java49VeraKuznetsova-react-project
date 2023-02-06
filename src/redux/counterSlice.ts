628528ee0b2aa76998cd1235866efbf490988c3b
Vera Kuznetsova <kuznetsova-vera@yandex.ru>
2023-02-06 14:19:56 +0200

    HW # 39 - only CW#39

--- src/redux/counterSlice.ts
+++ src/redux/counterSlice.ts
@@ -1,17 +1,27 @@
 import {createSlice} from '@reduxjs/toolkit';
 const initialState = {
-    count:0
+    count:10
 }
 const counterSlice = createSlice ({
       initialState: initialState,
       name: 'counter',
       reducers: {
-      increment: (state) => {state.count++;
+      increment: (state, data) => {
+            state.count+= data.payload;
                            },
-      decrement: state => {state.count--;
+      decrement: (state, data) => {
+              state.count-= data.payload;
                           },
       reset: state => {state.count = initialState.count;
                       }
               }
 
 })
+/*
+const ar: number[] = [1, 2, 3]
+ar.map((state) => state *2)
+*/
+
+export const counterActions = counterSlice.actions;
+export const counterReducer = counterSlice.reducer;
+
